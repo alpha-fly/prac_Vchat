c7dddb34fba2ffc1fec2b143401e57809c211729
alpha-fly <placetobe@naver.com>
2022-06-27 18:00:41 +0900

    addR

--- public/client.js
+++ public/client.js
@@ -22,11 +22,11 @@ let roomId
 // Free public STUN servers provided by Google.
 const iceServers = {
   "iceServers": [
-    { urls: 'stun:stun.l.google.com:19302' },
-    { urls: 'stun:stun1.l.google.com:19302' },
-    { urls: 'stun:stun2.l.google.com:19302' },
-    { urls: 'stun:stun3.l.google.com:19302' },
-    { urls: 'stun:stun4.l.google.com:19302' },
+    { url: 'stun:stun.l.google.com:19302' },
+    { url: 'stun:stun1.l.google.com:19302' },
+    { url: 'stun:stun2.l.google.com:19302' },
+    { url: 'stun:stun3.l.google.com:19302' },
+    { url: 'stun:stun4.l.google.com:19302' },
   ],
 }
 
